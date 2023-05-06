aa3ab1baca52ed4862a0f7889124906240c9c298
duykhanhrk <tranduykhanh.rk@gmail.com>
2023-05-06 22:54:35 +0700

    fix: rubocop

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::V1::SessionsController < ActionController::API
   include JwtAuth::Helpers
   include VerificationHelper
--- app/models/concerns/notificationable.rb
+++ app/models/concerns/notificationable.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Notificationable
   extend ActiveSupport::Concern
 
